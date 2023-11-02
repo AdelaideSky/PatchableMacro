0b1b59496e89d7803127146bfe114a3084f38583
Adélaïde Sky <augustin.humez@gmail.com>
2023-11-02 15:10:03 +0100

    xdfcgvhbj

--- Sources/Patchable/Dict+Patchable.swift
+++ Sources/Patchable/Dict+Patchable.swift
@@ -15,6 +15,21 @@ extension Dictionary: PatchableProtocol where Key == String, Value: PatchablePro
         try self[child]?.patch(path, with: value)
     }
 }
+
+extension Array: PatchableProtocol where Element: PatchableProtocol {
+    public func patch(_ key: String, with value: Data) throws {}
+    
+    public func patch(child: String, path: [String], with value: Data) throws {
+        if let index = Int(child) {
+            guard index < self.count else { return }
+            
+            try self[index].patch(path, with: value)
+        } else {
+            return
+        }
+        
+    }
+}
 //IM GONNA BREAK SOEMTHING YOU CAN'T FUCKING JUST HAVE 2 CONDITIONAL COMFORMANCE HELP AND ITS BEEN 1H I TRY TO FUCKING FIGURE THIS OUT SO HERE YOU GO YOU WON'T HAVE THE OPTION OK
 
 //
